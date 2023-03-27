2f5bf62a1a7e79e43a0d8931afb0933ec4a18cf2
Vivek Viradia <vivekviradia123@gmail.com>
2023-03-27 15:56:22 +0530

    Created new APi to update Products in product-updater.jsx

--- web/index.js
+++ web/index.js
@@ -65,6 +65,23 @@ app.get('/api/products', async (req, res) => {
 
   res.status(200).send({products})
 })
+//--------------------------------------------
+
+app.post("/api/products/update", async (_req, res) => {
+  let status = 200;
+  let error = null;
+
+  try {
+    await productCreator(res.locals.shopify.session);
+  } catch (e) {
+    console.log(`Failed to process products/create: ${e.message}`);
+    status = 500;
+    error = e.message;
+  }
+  res.status(status).send({ success: status === 200, error });
+});
+
+
 
 //-----------------------------
 app.get("/api/products/count", async (_req, res) => {
